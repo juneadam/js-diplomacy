0325102f96b2551a6d9c74c46c11e630305f48fe
June Balter <junedoeswork@gmail.com>
2023-01-11 11:45:40 -0600

    completed work

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const colorChange = document.querySelectorAll('.color-change');
+
+const colorButton = document.querySelector('.color-changer');
+
+colorButton.addEventListener('click', () => {
+    for (const element of colorChange) {
+        element.classList.add('red');
+    }
+});
+
+const numForm = document.querySelector('.number-form')
+
+numForm.addEventListener('submit', (evt) => { 
+    const numberInput = document.querySelector('input[name="number"]');
+
+    if (numberInput.value.length >= 10) {
+        evt.preventDefault()
+        document.querySelector("#formFeedback").innerHTML = "Please enter a smaller number.";
+    }
+    else if (numberInput.value.length < 10) {
+        evt.preventDefault()
+        document.querySelector("#formFeedback").innerHTML = " You are good to go!"
+    }
+});
